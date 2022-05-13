69ddb5b3c94b2059a4752265adcc09b972dd2412
bilalkarakollu <karakollubilal@gmail.com>
2022-05-13 22:09:37 +0300

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import User from './pages/User';
 
 const App = () => {
   return (
-    <div>
-      s
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="user" element={<User />} />
+    </Routes>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './App';
-import {
-    ApolloProvider
-  } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
 import client from './apollo/client';
+import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ApolloProvider client={client}><App /></ApolloProvider>);
+root.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
+);
 
