4712735560bb1c99bf1b054c0a1e975b537a1a8e
bilalkarakollu <karakollubilal@gmail.com>
2022-05-14 06:47:36 +0300

    Add redux

---
+++ src/features/userSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  users: [],
+}
+
+export const userSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.users = action.payload
+    },
+    setPuan: (state, action) => {
+        const user = state.users.find(user => user.id === action.payload)
+        user.puan += 1;
+        state.users.sort((a, b) => b.puan - a.puan);
+    }
+  },
+})
+
+export const { setUser, setPuan } = userSlice.actions
+
+export default userSlice.reducer
--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ import './styles/index.css';
 import { BrowserRouter } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
 import client from './apollo/client';
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
+  </Provider>
 );
 
